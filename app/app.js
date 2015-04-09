208f3217f0d085ba21a1864e0c33edb1ff55a352
tiagofernandesp <tiagofernandesp@gmail.com>
2015-04-09 18:30:10 +0100

    Final Project Consume Webservices

---
+++ app/app.js
@@ -0,0 +1,40 @@
+﻿var app = angular.module('escalasApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/escalas',
+            {
+                controller: 'EscalasController',
+                templateUrl: '/app/views/panelEscalas.html'
+            })
+        .when('/servicos',
+            {
+                controller: 'EscalasController',
+                templateUrl: '/app/views/panelEscalas.html'
+            })
+        .when('/pessoas',
+            {
+                controller: 'PessoasController',
+                templateUrl: '/app/views/panelPessoas.html'
+            })
+        .when('/pessoas/edit/:pessoaID',
+            {
+                controller: 'PessoasControllerEdit',
+                templateUrl: '/app/views/panelPessoasEdit.html'
+            })
+        .when('/pessoas/new/',
+            {
+                controller: 'PessoasControllerNew',
+                templateUrl: '/app/views/panelPessoasNew.html'
+            })
+        //Define a route that has a route parameter in it (:customerID)
+        //.when('/customerorders/:customerID',
+        //    {
+        //        controller: 'CustomerOrdersController',
+        //        templateUrl: '/app/partials/customerOrders.html'
+        //    })
+        //Define a route that has a route parameter in it (:customerID)
+        .otherwise({ redirectTo: '/pessoas' });
+});
+
+
---
+++ js/myJScripts.js
@@ -0,0 +1,4 @@
+﻿$("#dp").datepicker({
+    //changeMonth: true,
+    //changeYear: true
+});
